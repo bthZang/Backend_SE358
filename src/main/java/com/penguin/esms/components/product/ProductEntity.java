0d797395426574b6712ca7e99ab51238412aae17
SONLE03 <sonle102003@gmail.com>
2023-12-02 02:42:57 +0700

    GSOP-154 Add product entity

--- src/main/java/com/penguin/esms/components/product/ProductEntity.java
+++ src/main/java/com/penguin/esms/components/product/ProductEntity.java
@@ -1,16 +1,55 @@
 package com.penguin.esms.components.product;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.penguin.esms.components.category.CategoryEntity;
+import com.penguin.esms.components.supplier.SupplierEntity;
+import com.penguin.esms.entity.BaseEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.Date;
+import java.util.List;
+
 
 @Entity
 @Table
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
-@RequiredArgsConstructor
-public class ProductEntity {
+public class ProductEntity extends BaseEntity{
+    @Column(unique = true)
+    @NotNull
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    @JsonIgnoreProperties(value = {"products"})
+    private CategoryEntity category;
+    @ManyToMany
+    @JsonIgnoreProperties(value = {"products"})
+    private List<SupplierEntity> suppliers;
+    private String unit;
+    private Long price;
+    private Integer quantity;
+    private Integer warrantyPeriod;
+    private Boolean isAvailable = true;
+    private String photoURL;
 
+    @Override
+    public String toString() {
+        return "ProductEntity{" +
+                "name='" + name + '\'' +
+                (", category='" + (category != null ? category.getName() : "None")) + '\'' +
+                ", unit='" + unit + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", warrantyPeriod=" + warrantyPeriod +
+                ", isAvailable=" + isAvailable +
+                '}';
+    }
 }
