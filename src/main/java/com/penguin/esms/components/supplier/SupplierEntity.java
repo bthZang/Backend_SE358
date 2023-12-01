85b45fb0ddac11cc736cf590f04e0d4e3fdae9dd
SONLE03 <sonle102003@gmail.com>
2023-12-02 02:28:00 +0700

    GSOP-149 Add Supplier Entity

--- src/main/java/com/penguin/esms/components/supplier/SupplierEntity.java
+++ src/main/java/com/penguin/esms/components/supplier/SupplierEntity.java
@@ -1,4 +1,28 @@
 package com.penguin.esms.components.supplier;
 
-public class SupplierEntity {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.penguin.esms.components.product.ProductEntity;
+import com.penguin.esms.entity.BaseEntity;
+import com.penguin.esms.entity.NoteEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class SupplierEntity extends NoteEntity{
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+
+    @JsonIgnoreProperties(value = {"suppliers"})
+    @ManyToMany(mappedBy = "suppliers")
+    private List<ProductEntity> products;
 }
