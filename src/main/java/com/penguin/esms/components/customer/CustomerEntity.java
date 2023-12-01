1736f34564da8e14781ef6e694b647001db5f760
SONLE03 <sonle102003@gmail.com>
2023-12-02 02:11:01 +0700

    GSOP-148 Add Customer Entity

---
+++ src/main/java/com/penguin/esms/components/customer/CustomerEntity.java
@@ -0,0 +1,55 @@
+package com.penguin.esms.components.customer;
+
+import com.penguin.esms.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Table
+@Entity
+public class CustomerEntity extends BaseEntity {
+    private String name;
+    private String phone;
+    private String address;
+
+    public CustomerEntity(){
+
+    }
+    public CustomerEntity(String name, String phone, String address) {
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerEntity{" +
+                "name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
