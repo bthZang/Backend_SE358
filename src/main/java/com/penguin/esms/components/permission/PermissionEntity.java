cd210145539be61f1b64d4a2111246c51f96a9de
SONLE03 <sonle102003@gmail.com>
2023-12-02 17:08:19 +0700

    GSOP-153 Add Permission Type and Permission Entity

---
+++ src/main/java/com/penguin/esms/components/permission/PermissionEntity.java
@@ -0,0 +1,37 @@
+package com.penguin.esms.components.permission;
+
+import com.penguin.esms.components.staff.StaffEntity;
+import com.penguin.esms.entity.BaseEntity;
+import com.penguin.esms.entity.NoteEntity;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.hibernate.type.EntityType;
+
+import java.security.Permission;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table
+@RequiredArgsConstructor
+public class PermissionEntity {
+    @Enumerated(EnumType.STRING)
+    private PermissionType permissionType;
+    @Enumerated(EnumType.STRING)
+    private EntityType entityType;
+    private String entityId;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "staff_id")
+    @NotNull
+    private StaffEntity staff;
+
+    @Override
+    public String toString() {
+        return permissionType + ":" + entityType + (entityId != null ? ":" + entityId : "");
+    }
+}
---
+++ src/main/java/com/penguin/esms/components/permission/PermissionType.java
@@ -0,0 +1,8 @@
+package com.penguin.esms.components.permission;
+
+public enum PermissionType {
+    CREATE,
+    VIEW_ITEM, VIEW_ALL, VIEW_LIST,
+    UPDATE_ITEM, UPDATE_ALL,
+    DELETE_ITEM, DELETE_ALL
+}
