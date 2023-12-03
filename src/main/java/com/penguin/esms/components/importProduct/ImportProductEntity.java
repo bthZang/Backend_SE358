b8662069091c4a959546070d1b62d444235d69f0
SONLE03 <sonle102003@gmail.com>
2023-12-04 01:31:47 +0700

    GSOP-159 GSOP-160: Add Import Product Entity and Sale Product Entity

--- src/main/java/com/penguin/esms/components/importProduct/ImportProductEntity.java
+++ src/main/java/com/penguin/esms/components/importProduct/ImportProductEntity.java
@@ -1,2 +1,85 @@
-package com.penguin.esms.components.importProduct;public class ImportProductEntity {
+package com.penguin.esms.components.importProduct;
+
+import com.penguin.esms.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Table
+@Entity
+public class ImportProductEntity extends BaseEntity {
+    private String productId;
+    private String staffId;
+    private Integer quantity;
+    private Integer index;
+    private Long price;
+    private String unit;
+
+    public ImportProductEntity(String productId, String staffId, Integer quantity, Integer index, Long price, String unit) {
+        this.productId = productId;
+        this.staffId = staffId;
+        this.quantity = quantity;
+        this.index = index;
+        this.price = price;
+        this.unit = unit;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getStaffId() {
+        return staffId;
+    }
+
+    public void setStaffId(String staffId) {
+        this.staffId = staffId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    @Override
+    public String toString() {
+        return "ImportProductEntity{" +
+                "productId='" + productId + '\'' +
+                ", staffId='" + staffId + '\'' +
+                ", quantity=" + quantity +
+                ", index=" + index +
+                ", price=" + price +
+                ", unit='" + unit + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/penguin/esms/components/saleProduct/SaleProductEntity.java
+++ src/main/java/com/penguin/esms/components/saleProduct/SaleProductEntity.java
@@ -1,2 +1,85 @@
-package com.penguin.esms.components.saleProduct;public class SaleProductEntity {
+package com.penguin.esms.components.saleProduct;
+
+import com.penguin.esms.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class SaleProductEntity extends BaseEntity {
+    private String productId;
+    private String saleBillId;
+    private Integer quantity;
+    private Integer index;
+    private Long price;
+    private String unit;
+
+    public SaleProductEntity(String productId, String saleBillId, Integer quantity, Integer index, Long price, String unit) {
+        this.productId = productId;
+        this.saleBillId = saleBillId;
+        this.quantity = quantity;
+        this.index = index;
+        this.price = price;
+        this.unit = unit;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getSaleBillId() {
+        return saleBillId;
+    }
+
+    public void setSaleBillId(String saleBillId) {
+        this.saleBillId = saleBillId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    @Override
+    public String toString() {
+        return "SaleProductEntity{" +
+                "productId='" + productId + '\'' +
+                ", saleBillId='" + saleBillId + '\'' +
+                ", quantity=" + quantity +
+                ", index=" + index +
+                ", price=" + price +
+                ", unit='" + unit + '\'' +
+                '}';
+    }
 }
