df20d304b2fc8dce0ab4db6073911f6d50cac31d
SONLE03 <sonle102003@gmail.com>
2023-12-04 02:59:03 +0700

    GSOP-99 Add Mapper Construct to map from DTO to entity

---
+++ src/main/java/com/penguin/esms/mapper/DTOtoEntityMapper.java
@@ -0,0 +1,32 @@
+package com.penguin.esms.mapper;
+
+import com.penguin.esms.components.category.CategoryDTO;
+import com.penguin.esms.components.category.CategoryEntity;
+import com.penguin.esms.components.product.ProductDTO;
+import com.penguin.esms.components.product.ProductEntity;
+import com.penguin.esms.components.staff.StaffDTO;
+import com.penguin.esms.components.staff.StaffEntity;
+import com.penguin.esms.components.supplier.SupplierEntity;
+import com.penguin.esms.components.supplier.dto.SupplierDTO;
+import org.mapstruct.*;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface DTOtoEntityMapper {
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateCategoryFromDto(CategoryDTO dto, @MappingTarget CategoryEntity entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateProductFromDto(ProductDTO dto, @MappingTarget ProductEntity entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateStaffFromDto(StaffDTO dto, @MappingTarget StaffEntity entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateSupplierFromDto(SupplierDTO dto, @MappingTarget SupplierEntity entity);
+
+    default List<SupplierEntity> map(List<String> value) {
+        return value.stream().map(v -> new SupplierEntity()).toList();
+    }
+}
