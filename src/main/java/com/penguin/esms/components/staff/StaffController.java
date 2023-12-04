d1783d3136bd0537b3bba3513bc8c44b11f9d965
SONLE03 <sonle102003@gmail.com>
2023-12-05 00:39:13 +0700

    GSOP-136 Add feature to modify employee information

--- src/main/java/com/penguin/esms/components/staff/StaffController.java
+++ src/main/java/com/penguin/esms/components/staff/StaffController.java
@@ -4,10 +4,7 @@ import com.penguin.esms.components.staff.requests.NewStaffRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -16,10 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class StaffController {
 
     private final StaffRepository staffRepository;
+    private final StaffService staffService;
 
     @PostMapping("")
     @PreAuthorize("hasAuthority('CREATE:STAFF') or hasAuthority('ADMIN')")
     public ResponseEntity<?> createStaff(@RequestBody NewStaffRequest newStaff) {
         return ResponseEntity.ok(staffRepository.save(new StaffEntity(newStaff.getName(), newStaff.getPhone(), newStaff.getPassword(), newStaff.getEmail(), newStaff.getCitizenId(), newStaff.getRole())));
+    }
+    @PutMapping(path = "{id}")
+    public ResponseEntity<?>  edit(@RequestBody StaffDTO staffDTO, @PathVariable String id) {
+        return ResponseEntity.ok(staffService.update(staffDTO, id));
     }
 }
--- src/main/java/com/penguin/esms/components/staff/StaffService.java
+++ src/main/java/com/penguin/esms/components/staff/StaffService.java
@@ -1,11 +1,26 @@
 package com.penguin.esms.components.staff;
 
+import com.penguin.esms.mapper.DTOtoEntityMapper;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
 public class StaffService {
+    private final StaffRepository staffRepository;
+    private final DTOtoEntityMapper mapper;
 
+    public StaffEntity update(StaffDTO staffDTO, String id){
+        if (staffRepository.findById(id).isEmpty())
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Staff not existed");
+        StaffEntity staff = staffRepository.findById(id).get();
+        mapper.updateStaffFromDto(staffDTO, staff);
+        return staffRepository.save(staff);
+    }
 }
