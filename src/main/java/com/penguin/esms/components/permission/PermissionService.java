a2e218182862cfb9a4f03eaf128f81918422a558
Anh Việt <149693120+AnhViet0307@users.noreply.github.com>
2023-12-13 15:08:45 +0700

    Gsop 127 write api for removing permission (#9)
    * GSOP-126 Add Permission Controller
    
    * (GSOP-125) Update Permission Repo
    
    * (GSOP-125) Add Get-Permission to Permission Service
    
    * (GSOP-126) Implement Add-Permission to Permission Controller
    
    * GSOP-128 Implement Permission Removing Feature to Service
    
    * GSOP-129 Implement Removing Permission & Write Authorization to Controller
    
    ---------
    
    Co-authored-by: Lê Hồng Sơn <92604333+SONLE03@users.noreply.github.com>

--- src/main/java/com/penguin/esms/components/permission/PermissionService.java
+++ src/main/java/com/penguin/esms/components/permission/PermissionService.java
@@ -1,10 +1,33 @@
 package com.penguin.esms.components.permission;
 
+import com.penguin.esms.components.permission.dto.PermissionRequest;
+import com.penguin.esms.components.staff.StaffEntity;
+import com.penguin.esms.components.staff.StaffRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class PermissionService {
     private final PermissionRepo permissionRepo;
+    private final StaffRepository staffRepository;
+
+    public PermissionEntity add(PermissionRequest permissionRequest, String staffId) {
+        Optional<StaffEntity> staff = staffRepository.findById(staffId);
+        PermissionEntity permission = new PermissionEntity();
+        permission.setPermissionType(permissionRequest.getPermissionType());
+        permission.setEntityType(permissionRequest.getEntityType());
+        permission.setEntityId(permissionRequest.getEntityId());
+        staff.ifPresent(permission::setStaff);
+        return permissionRepo.save(permission);
+    }
+
+    public void remove(String permissionId) {
+        Optional<PermissionEntity> permission = permissionRepo.findById(permissionId);
+        permission.ifPresent(permissionRepo::delete);
+        permissionRepo.findById(permissionId);
+    }
+
 }
