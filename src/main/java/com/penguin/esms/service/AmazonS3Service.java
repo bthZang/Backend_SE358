cd785b15e6773c197a83ab72fe6051906909d537
Zang <bt.hoanggiang@gmail.com>
2023-12-28 04:04:58 +0700

    save photo for product

---
+++ src/main/java/com/penguin/esms/service/AmazonS3Service.java
@@ -0,0 +1,43 @@
+package com.penguin.esms.service;
+
+import com.penguin.esms.utils.AwsS3Client;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.regions.Region;
+
+import java.io.IOException;
+
+@Service
+public class AmazonS3Service {
+    @Value("${amazonProperties.bucket-name}")
+    private String AWS_BUCKET;
+    public Region AWS_REGION = Region.US_EAST_1;
+    AwsS3Client s3Service;
+
+    public AmazonS3Service() {
+        s3Service = new AwsS3Client(AWS_REGION);
+    }
+
+    public boolean isExistFile(String fileName) {
+        return s3Service.doesObjectExistByDefaultMethod(AWS_BUCKET, fileName);
+    }
+
+    public String addFile(MultipartFile file, String filename) throws IOException {
+        s3Service.putObject(AWS_BUCKET, filename, file);
+        return getObjectURL(filename);
+    }
+
+    public String updateFile(MultipartFile file, String filename) throws IOException {
+        s3Service.putObject(AWS_BUCKET, filename, file);
+        return getObjectURL(filename);
+    }
+
+    public void deleteFile(String fileName) {
+        s3Service.deleteObject(AWS_BUCKET, fileName);
+    }
+
+    public String  getObjectURL(String objectKey) {
+        return "https://" + AWS_BUCKET + ".s3.amazonaws.com/" + objectKey;
+    }
+}
