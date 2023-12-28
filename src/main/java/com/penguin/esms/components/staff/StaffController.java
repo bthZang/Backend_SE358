de56197837a8d69c8eae206ebce65063a1c2a5da
AnhViet <anhviet0307@gmail.com>
2023-12-28 22:54:47 +0700

    GSOP-269 Refactor view staff

--- src/main/java/com/penguin/esms/components/staff/StaffController.java
+++ src/main/java/com/penguin/esms/components/staff/StaffController.java
@@ -24,21 +24,27 @@ public class StaffController {
     private final StaffService staffService;
 
     @GetMapping("profile")
-    public ResponseEntity<?> getStaffProfile(Principal connectedUser){
+    public ResponseEntity<?> getStaffProfile(Principal connectedUser) {
         StaffEntity staff = (StaffEntity) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
         return ResponseEntity.ok(staff);
     }
-
-    @GetMapping("")
-    public ResponseEntity<?> getList(@RequestParam(defaultValue = "") String name) {
-        return ResponseEntity.ok(staffService.findByName(name));
+    @GetMapping("resigned")
+    @PreAuthorize("hasAuthority('VIEW_ITEM:STAFF:' + #id) or hasAuthority('VIEW_ALL:STAFF') or hasAuthority('ADMIN')")
+    public List<StaffEntity> getAllResigned(@RequestParam(defaultValue = "") String name) {
+        return staffService.findResigned(name);
     }
 
+    
+    @GetMapping
+    @PreAuthorize("hasAuthority('VIEW_ITEM:STAFF:' + #id) or hasAuthority('VIEW_ALL:STAFF') or hasAuthority('ADMIN')")
+    public List<StaffEntity> getAll(@RequestParam(defaultValue = "") String name) {
+        return staffService.findByName(name);
+    }
+  
     @GetMapping("{id}")
     @PreAuthorize("hasAuthority('VIEW_ITEM:STAFF:' + #id) or hasAuthority('VIEW_ALL:STAFF') or hasAuthority('ADMIN')")
-    public ResponseEntity<?> getStaffById(@PathVariable String id) {
-        if (staffRepository.findById(id).isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        return ResponseEntity.ok(staffRepository.findById(id));
+    public StaffEntity getStaffById(@PathVariable String id) {
+        return staffService.getOne(id);
     }
 
     @PostMapping("")
