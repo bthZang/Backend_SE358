400fe18570cf91f3257454369e8fc6bd44ba7444
AnhViet <anhviet0307@gmail.com>
2023-12-29 16:44:16 +0700

    GSOP-269 Refactor view staff in Staff Repo

--- src/main/java/com/penguin/esms/components/staff/StaffRepository.java
+++ src/main/java/com/penguin/esms/components/staff/StaffRepository.java
@@ -10,5 +10,8 @@ import java.util.Optional;
 public interface StaffRepository extends JpaRepository<StaffEntity, String> {
     List<StaffEntity> findByNameContainingIgnoreCase(String name);
     Optional<StaffEntity> findByEmail(String email);
+    Optional<StaffEntity> findByName(String name);
+    public List<StaffEntity> findByNameContainingIgnoreCaseAndIsStopped(String name, boolean isStopped);
     Optional<StaffEntity> findById(String id);
+    Optional<StaffEntity> findByCitizenId(String citizenId);
 }
