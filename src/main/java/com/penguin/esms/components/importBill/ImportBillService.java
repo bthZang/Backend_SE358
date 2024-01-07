f8762c6aaabec39818809b51849ecd56db69115b
AnhViet <anhviet0307@gmail.com>
2024-01-07 18:39:24 +0700

    GSOP-287 Add service for view import list in period

--- src/main/java/com/penguin/esms/components/importBill/ImportBillService.java
+++ src/main/java/com/penguin/esms/components/importBill/ImportBillService.java
@@ -22,9 +22,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -130,4 +128,45 @@ public class ImportBillService {
         entityManager.close();
         return audit;
     }
+
+    @Transactional
+    public List<?> getAllRevisions(Date start, Date end) {
+        AuditReader auditReader = AuditReaderFactory.get(entityManager);
+
+        AuditQuery query = auditReader.createQuery()
+                .forRevisionsOfEntity(ImportBillEntity.class, true, true)
+                .addProjection(AuditEntity.revisionNumber())
+                .addProjection(AuditEntity.property("staffId"))
+                .addProjection(AuditEntity.property("supplierId"))
+                .addProjection(AuditEntity.property("paymentMethod"))
+                .addProjection(AuditEntity.property("id"))
+                .addProjection(AuditEntity.revisionType())
+                .addOrder(AuditEntity.revisionNumber().desc());
+
+        List<AuditEnversInfo> audit = new ArrayList<AuditEnversInfo>();
+        List<Object[]> objects = query.getResultList();
+        for(int i=0; i< objects.size();i++){
+            Object[] objArray = objects.get(i);
+            Optional<AuditEnversInfo> auditEnversInfoOptional = auditEnversInfoRepo.findById((int) objArray[0]);
+            if (auditEnversInfoOptional.isPresent()) {
+                AuditEnversInfo auditEnversInfo = auditEnversInfoOptional.get();
+//                ImportBillEntity entity = new ImportBillEntity((String) objArray[1],  (String) objArray[2], (String) objArray[3]);
+                ImportBillEntity entity = importBillRepo.findById((String) objArray[4]).get();
+                List<ImportProductEntity> importProducts = importProductRepo.findByImportBillId(entity.getId());
+                entity.setImportProducts(importProducts);
+                auditEnversInfo.setRevision(entity);
+                audit.add(auditEnversInfo);
+            }
+        }
+        Map<String, AuditEnversInfo> auditReturn = new HashMap<>();
+        for(int i=0; i< audit.size();i++){
+            if (audit.get(i).getTimestamp() > start.getTime() && audit.get(i).getTimestamp() < end.getTime() ) {
+                ImportBillEntity importBillEntity = (ImportBillEntity) audit.get(i).getRevision();
+                auditReturn.put(importBillEntity.getId(), audit.get(i));
+            }
+        }
+        entityManager.close();
+        return Arrays.asList(auditReturn.values().toArray());
+    }
+
 }
