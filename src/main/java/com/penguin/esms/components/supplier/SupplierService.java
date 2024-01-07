7b4336f65fdb8fe6457d667e3b6eaef70ce0693a
AnhViet <anhviet0307@gmail.com>
2024-01-07 10:37:30 +0700

    GSOP-283 Add Servicer for viewing supplier change history

--- src/main/java/com/penguin/esms/components/supplier/SupplierService.java
+++ src/main/java/com/penguin/esms/components/supplier/SupplierService.java
@@ -1,6 +1,14 @@
 package com.penguin.esms.components.supplier;
 
 import com.penguin.esms.components.supplier.dto.SupplierDTO;
+import com.penguin.esms.envers.AuditEnversInfo;
+import com.penguin.esms.envers.AuditEnversInfoRepo;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.hibernate.envers.AuditReader;
+import org.hibernate.envers.AuditReaderFactory;
+import org.hibernate.envers.query.AuditEntity;
+import org.hibernate.envers.query.AuditQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.penguin.esms.entity.Error;
 import com.penguin.esms.mapper.DTOtoEntityMapper;
@@ -17,6 +25,9 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class SupplierService {
+    @PersistenceContext
+    private final EntityManager entityManager;
+    private final AuditEnversInfoRepo auditEnversInfoRepo;
     private final SupplierRepo supplierRepo;
     private final DTOtoEntityMapper mapper;
 
@@ -77,5 +88,36 @@ public class SupplierService {
         }
         supplierEntityOptional.get().setIsStopped(true);
         supplierRepo.save(supplierEntityOptional.get());
+    }
+
+    @Transactional
+    public List<?> getRevisions(String id) {
+        AuditReader auditReader = AuditReaderFactory.get(entityManager);
+
+        AuditQuery query = auditReader.createQuery()
+                .forRevisionsOfEntity(SupplierEntity.class, true, true)
+                .add(AuditEntity.id().eq(id))
+                .addProjection(AuditEntity.revisionNumber())
+                .addProjection(AuditEntity.property("name"))
+                .addProjection(AuditEntity.property("phone"))
+                .addProjection(AuditEntity.property("email"))
+                .addProjection(AuditEntity.property("address"))
+                .addProjection(AuditEntity.revisionType())
+                .addOrder(AuditEntity.revisionNumber().desc());
+
+        List<AuditEnversInfo> audit = new ArrayList<AuditEnversInfo>();
+        List<Object[]> objects = query.getResultList();
+        for(int i=0; i< objects.size();i++){
+            Object[] objArray = objects.get(i);
+            Optional<AuditEnversInfo> auditEnversInfoOptional = auditEnversInfoRepo.findById((int) objArray[0]);
+            if (auditEnversInfoOptional.isPresent()) {
+                AuditEnversInfo auditEnversInfo = auditEnversInfoOptional.get();
+                SupplierDTO dto = new SupplierDTO((String) objArray[1],  (String) objArray[2], (String) objArray[3], (String) objArray[4]);
+                auditEnversInfo.setRevision(dto);
+                audit.add(auditEnversInfo);
+            }
+        }
+        entityManager.close();
+        return audit;
     }
 }
