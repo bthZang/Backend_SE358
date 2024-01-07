8790410b89c1ab67bda24905cf768aa80bdc76df
AnhViet <anhviet0307@gmail.com>
2024-01-07 18:41:09 +0700

    GSOP-256 Add controller for view import list

--- src/main/java/com/penguin/esms/components/importBill/ImportBillController.java
+++ src/main/java/com/penguin/esms/components/importBill/ImportBillController.java
@@ -7,15 +7,31 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.Date;
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "import")
 @RequiredArgsConstructor
 public class ImportBillController {
     private final ImportBillService importBillService;
+    @GetMapping("{id}")
+    public List<?> get(@PathVariable String id) {
+        return importBillService.getRevisions(id);
+    }
+    @GetMapping()
+    public List<?> get() {
+        return importBillService.getAll();
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<?> post(@RequestBody ImportBillDTO importBillDTO, Principal connectedUser) {
         return ResponseEntity.ok(importBillService.postImportBill(importBillDTO, connectedUser));
+    }
+    @GetMapping("history")
+    public ResponseEntity<?> getAll(@RequestParam long start, @RequestParam long end) {
+        if (start == end) return ResponseEntity.ok(importBillService.getAll());
+        return ResponseEntity.ok(importBillService.getAllRevisions(new Date(start), new Date(end)));
     }
 }
