2427aafb4bf0b3d5bfbd674800eb9d18cbb1e064
Zang <bt.hoanggiang@gmail.com>
2024-01-14 00:10:16 +0700

    refactor customer entity

--- src/main/java/com/penguin/esms/components/customer/CustomerEntity.java
+++ src/main/java/com/penguin/esms/components/customer/CustomerEntity.java
@@ -1,57 +1,25 @@
 package com.penguin.esms.components.customer;
 
+import com.penguin.esms.components.staff.validators.PhoneNumberFormat;
 import com.penguin.esms.entity.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.hibernate.envers.Audited;
 
-@Table
+import java.util.List;
+
 @Entity
+@Table
+@Getter
+@Setter
+@RequiredArgsConstructor
 @Audited
 public class CustomerEntity extends BaseEntity {
     private String name;
+    @Column(unique=true)
+    @PhoneNumberFormat(message = "Invalid phone number")
     private String phone;
     private String address;
-
-    public CustomerEntity(){
-
-    }
-    public CustomerEntity(String name, String phone, String address) {
-        this.name = name;
-        this.phone = phone;
-        this.address = address;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    @Override
-    public String toString() {
-        return "CustomerEntity{" +
-                "name='" + name + '\'' +
-                ", phone='" + phone + '\'' +
-                ", address='" + address + '\'' +
-                '}';
-    }
 }
