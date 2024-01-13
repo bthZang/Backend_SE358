b29772de07b0ee7d35f8601fa6abf23639396650
Zang <bt.hoanggiang@gmail.com>
2024-01-14 01:47:08 +0700

    GSOP-359 add optional in customer

--- src/main/java/com/penguin/esms/components/customer/CustomerRepo.java
+++ src/main/java/com/penguin/esms/components/customer/CustomerRepo.java
@@ -3,6 +3,10 @@ package com.penguin.esms.components.customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepo extends JpaRepository<CustomerEntity, String> {
+    Optional<CustomerEntity> findByPhone(String phone);
+
 }
