67e3b0ad9c5c90f6fdae3615ff9d39d91becd4ee
Zang <bt.hoanggiang@gmail.com>
2024-01-13 22:18:06 +0700

    GSOP-347 override addCorsMappings

--- src/main/java/com/penguin/esms/config/WebConfig.java
+++ src/main/java/com/penguin/esms/config/WebConfig.java
@@ -1,6 +1,17 @@
 package com.penguin.esms.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOriginPatterns("*").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true).maxAge(3600);
+    }
 }
