ec9dae58b694ffc8c11b4c65909aab6fa92603c2
Zang <bt.hoanggiang@gmail.com>
2024-01-14 22:28:52 +0700

    GSOP-351 refactor SaleBillEntity

--- src/main/java/com/penguin/esms/components/saleBill/SaleBillEntity.java
+++ src/main/java/com/penguin/esms/components/saleBill/SaleBillEntity.java
@@ -2,6 +2,7 @@ package com.penguin.esms.components.saleBill;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.penguin.esms.components.customer.CustomerEntity;
+import com.penguin.esms.components.saleProduct.SaleProductEntity;
 import com.penguin.esms.entity.NoteEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -11,6 +12,7 @@ import org.hibernate.envers.Audited;
 import org.hibernate.envers.NotAudited;
 
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table
@@ -22,10 +24,11 @@ public class SaleBillEntity extends NoteEntity {
     private String staffId;
     private String paymentMethod;
     private Float discount;
-
+    @JsonIgnoreProperties(value = {"saleBill"})
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "saleBill")
+    private List<SaleProductEntity> saleProducts;
     @ManyToOne
     @JoinColumn(name = "customer_id")
     @JsonIgnoreProperties(value = {"saleBills"})
     private CustomerEntity customer;
-
 }
