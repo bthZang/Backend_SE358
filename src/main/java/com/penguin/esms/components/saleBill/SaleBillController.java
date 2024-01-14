68af7044ca78c5ca51a34c6154657666a01c6d20
Zang <bt.hoanggiang@gmail.com>
2024-01-14 22:47:15 +0700

    GSOP-349 add sale bill

--- src/main/java/com/penguin/esms/components/saleBill/SaleBillController.java
+++ src/main/java/com/penguin/esms/components/saleBill/SaleBillController.java
@@ -1,9 +1,16 @@
 package com.penguin.esms.components.saleBill;
 
+import com.penguin.esms.components.saleBill.dto.SaleBillDTO;
+import com.penguin.esms.components.staff.StaffEntity;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping(path = "sale")
@@ -12,4 +19,10 @@ import org.springframework.web.bind.annotation.*;
 @Setter
 public class SaleBillController {
     private final SaleBillService saleBillService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> post(@RequestBody SaleBillDTO dto, Principal connectedUser) {
+        StaffEntity staff = (StaffEntity) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
+        return ResponseEntity.ok(saleBillService.post(dto, staff));
+    }
 }
