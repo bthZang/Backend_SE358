6bbceac761e2017dd343776f4cd6917ee083c4bd
Zang <bt.hoanggiang@gmail.com>
2024-01-15 02:53:35 +0700

    GSOP-349 update add sale bill

--- src/main/java/com/penguin/esms/mapper/DTOtoEntityMapper.java
+++ src/main/java/com/penguin/esms/mapper/DTOtoEntityMapper.java
@@ -10,6 +10,10 @@ import com.penguin.esms.components.importProduct.ImportProductEntity;
 import com.penguin.esms.components.importProduct.dto.ImportProductDTO;
 import com.penguin.esms.components.product.dto.ProductDTO;
 import com.penguin.esms.components.product.ProductEntity;
+import com.penguin.esms.components.saleBill.SaleBillEntity;
+import com.penguin.esms.components.saleBill.dto.SaleBillDTO;
+import com.penguin.esms.components.saleProduct.SaleProductEntity;
+import com.penguin.esms.components.saleProduct.dto.SaleProductDTO;
 import com.penguin.esms.components.staff.StaffDTO;
 import com.penguin.esms.components.staff.StaffEntity;
 import com.penguin.esms.components.supplier.SupplierEntity;
@@ -37,6 +41,10 @@ public interface DTOtoEntityMapper {
     void updateImportBillFromDto(ImportBillDTO dto, @MappingTarget ImportBillEntity entity);
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateImportProductFromDto(ImportProductDTO dto, @MappingTarget ImportProductEntity entity);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateSaleBillFromDto(SaleBillDTO dto, @MappingTarget SaleBillEntity entity);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateSaleProductFromDto(SaleProductDTO dto, @MappingTarget SaleProductEntity entity);
     default List<SupplierEntity> map(List<String> value) {
         return value.stream().map(v -> new SupplierEntity()).toList();
     }
