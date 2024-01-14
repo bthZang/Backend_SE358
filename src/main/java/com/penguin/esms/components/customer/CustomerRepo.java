f44657256a0f6c6b5ee2e474973591f36afd26b6
Zang <bt.hoanggiang@gmail.com>
2024-01-14 20:27:16 +0700

    GSOP-359 get customer by id

--- src/main/java/com/penguin/esms/components/customer/CustomerRepo.java
+++ src/main/java/com/penguin/esms/components/customer/CustomerRepo.java
@@ -3,10 +3,15 @@ package com.penguin.esms.components.customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CustomerRepo extends JpaRepository<CustomerEntity, String> {
+    Optional<CustomerEntity> findByName(String name);
+    List<CustomerEntity> findByNameContainingIgnoreCaseAndPhoneContainingIgnoreCaseAndIsStopped(String name, String phone, boolean isStopped);
+    List<CustomerEntity> findByNameContainingIgnoreCaseAndIsStopped(String name, boolean isStopped);
+    Optional<CustomerEntity> findById(String id);
     Optional<CustomerEntity> findByPhone(String phone);
-
+    List<CustomerEntity> findByPhoneContainingIgnoreCase(String phone);
 }
