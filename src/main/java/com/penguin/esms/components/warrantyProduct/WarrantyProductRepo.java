12077b3c8940f98ab02ed4375cf9f12b36730bd5
Zang <bt.hoanggiang@gmail.com>
2024-01-17 01:47:45 +0700

    GSOP-357 get warranty bill by id method in service

--- src/main/java/com/penguin/esms/components/warrantyProduct/WarrantyProductRepo.java
+++ src/main/java/com/penguin/esms/components/warrantyProduct/WarrantyProductRepo.java
@@ -3,6 +3,12 @@ package com.penguin.esms.components.warrantyProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface WarrantyProductRepo extends JpaRepository<WarrantyProductEntity, String> {
+    Optional<WarrantyProductEntity> findById(String id);
+    List<WarrantyProductEntity> findByWarrantyBillId(String warrantyBillId);
+
 }
