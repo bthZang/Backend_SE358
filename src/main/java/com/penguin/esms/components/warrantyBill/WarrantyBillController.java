d992fb0d04b455a23cb6c9b31c17fff0e207792f
Zang <bt.hoanggiang@gmail.com>
2024-01-17 01:41:18 +0700

    GSOP-358 post warranty bill

--- src/main/java/com/penguin/esms/components/warrantyBill/WarrantyBillController.java
+++ src/main/java/com/penguin/esms/components/warrantyBill/WarrantyBillController.java
@@ -1,11 +1,24 @@
 package com.penguin.esms.components.warrantyBill;
 
+import com.penguin.esms.components.staff.StaffEntity;
+import com.penguin.esms.components.warrantyBill.dto.WarrantyBillDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping(path = "warranty")
 @RequiredArgsConstructor
 public class WarrantyBillController {
     private final WarrantyBillService warrantyBillService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> post(@RequestBody WarrantyBillDTO dto, Principal connectedUser) {
+        StaffEntity staff = (StaffEntity) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
+        return ResponseEntity.ok(warrantyBillService.postWarrantyBill(dto,staff));
+    }
 }
