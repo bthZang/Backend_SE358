bc691f6d8320158a30587631ecc168a5669afc3d
Zang <bt.hoanggiang@gmail.com>
2024-01-18 03:31:24 +0700

    update pom

--- src/main/java/com/penguin/esms/components/authentication/AuthenticationController.java
+++ src/main/java/com/penguin/esms/components/authentication/AuthenticationController.java
@@ -3,17 +3,20 @@ package com.penguin.esms.components.authentication;
 import com.penguin.esms.components.authentication.requests.AuthenticationRequest;
 import com.penguin.esms.components.authentication.requests.RegisterRequest;
 import com.penguin.esms.components.authentication.responses.AuthenticationResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.sql.SQLException;
 
 @RestController
 @RequestMapping("auth")
 @RequiredArgsConstructor
+@ControllerAdvice
 public class AuthenticationController {
 
     private final AuthenticationService service;
@@ -30,5 +33,18 @@ public class AuthenticationController {
             @RequestBody AuthenticationRequest request
     ) {
         return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @PostMapping("refresh-token")
+    public void refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        service.refreshToken(request, response);
+    }
+
+    @ExceptionHandler({ SQLException.class })
+    public ResponseEntity<?> handleSQLException(SQLException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
