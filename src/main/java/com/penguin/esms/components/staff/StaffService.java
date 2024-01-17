b6417892e4de956b68129433cab8791a510285ce
Zang <bt.hoanggiang@gmail.com>
2024-01-17 18:48:30 +0700

    GSOP-346 Add staff

--- src/main/java/com/penguin/esms/components/staff/StaffService.java
+++ src/main/java/com/penguin/esms/components/staff/StaffService.java
@@ -1,11 +1,13 @@
 package com.penguin.esms.components.staff;
 
+import com.penguin.esms.entity.Error;
 import com.penguin.esms.mapper.DTOtoEntityMapper;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.server.ResponseStatusException;
@@ -16,17 +18,37 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static com.penguin.esms.utils.Random.random;
+
 @Service
 @RequiredArgsConstructor
 public class StaffService {
-//    private final EntityManager entityManager;
     private final StaffRepository staffRepository;
     private final DTOtoEntityMapper mapper;
+    private final PasswordEncoder passwordEncoder;
+
 
     public void getStaffProfile(Principal connectedUser){
         StaffEntity staff = (StaffEntity) ((UsernamePasswordAuthenticationToken)connectedUser).getPrincipal();
     }
-
+    public StaffEntity addStaff(StaffDTO dto) throws Exception {
+        Optional<StaffEntity> optional = staffRepository.findByCitizenId(dto.getCitizenId());
+        if (optional.isPresent()) {
+            if (optional.get().getIsStopped() == true)
+                throw new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, new Error("Staff has resigned").toString());
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, new Error("Staff has been existed").toString());
+        }
+        StaffEntity staff = updateFromDTO(dto, new StaffEntity());
+        staff.setIsStopped(false);
+        String password = staff.getEmail();
+        staff.setPassword(passwordEncoder.encode(password));
+        return staffRepository.save(staff);
+    }
+    private StaffEntity updateFromDTO(StaffDTO dto, StaffEntity staff) {
+        mapper.updateStaffFromDto(dto, staff);
+        return staff;
+    }
     public StaffEntity update(StaffDTO staffDTO, String id){
         StaffEntity staff = staffRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(
@@ -35,7 +57,7 @@ public class StaffService {
         mapper.updateStaffFromDto(staffDTO, staff);
         return staffRepository.save(staff);
     }
-    
+
     public void remove(String id) {
         Optional<StaffEntity> staff = staffRepository.findById(id);
         if (staff.isEmpty())
@@ -45,7 +67,7 @@ public class StaffService {
         staffRepository.save(staff.get());
     }
 
-     public List<StaffEntity> findByName(String name) {
+    public List<StaffEntity> findByName(String name) {
         return staffRepository.findByNameContainingIgnoreCaseAndIsStopped(name, false);
     }
 
@@ -53,7 +75,7 @@ public class StaffService {
         return staffRepository.findByNameContainingIgnoreCaseAndIsStopped(name, true);
     }
 
-        public StaffEntity getOne(String id) {
+    public StaffEntity getOne(String id) {
         Optional<StaffEntity> staff = staffRepository.findById(id);
         if (staff.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, new Error("Staff not existed").toString());
