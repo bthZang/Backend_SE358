c0a597a4eeb19e68be16822ae3a10b6916a281d5
Zang <bt.hoanggiang@gmail.com>
2024-01-18 03:38:55 +0700

    GSOP-369 get method of staff

--- src/main/java/com/penguin/esms/components/staff/StaffController.java
+++ src/main/java/com/penguin/esms/components/staff/StaffController.java
@@ -1,6 +1,7 @@
 package com.penguin.esms.components.staff;
 
-import com.penguin.esms.components.staff.requests.NewStaffRequest;
+import com.penguin.esms.components.permission.PermissionService;
+import com.penguin.esms.components.permission.dto.StaffPermissionResponse;
 import com.penguin.esms.services.AmazonS3Service;
 import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
@@ -10,16 +11,19 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.security.Principal;
 import java.util.List;
 
 @RestController
 @RequestMapping("staff")
 @RequiredArgsConstructor
+@ControllerAdvice
 public class StaffController {
 
     private final StaffRepository staffRepository;
     private final StaffService staffService;
+    private final PermissionService permissionService;
     private final AmazonS3Service amazonS3Service;
 
     @PostMapping("")
@@ -41,33 +45,41 @@ public class StaffController {
     public List<StaffEntity> getAllResigned(@RequestParam(defaultValue = "") String name) {
         return staffService.findResigned(name);
     }
-
-    
+    @GetMapping("history/{id}")
+    public List<?> getALlHistory(@PathVariable String id) {
+        return staffService.getRevisionsForStaff(id);
+    }
     @GetMapping
     @PreAuthorize("hasAuthority('VIEW_ITEM:STAFF:' + #id) or hasAuthority('VIEW_ALL:STAFF') or hasAuthority('ADMIN')")
     public List<StaffEntity> getAll(@RequestParam(defaultValue = "") String name) {
         return staffService.findByName(name);
     }
-  
+
+    @GetMapping("username")
+    @PreAuthorize("hasAuthority('VIEW_ITEM:STAFF:' + #id) or hasAuthority('VIEW_ALL:STAFF') or hasAuthority('ADMIN')")
+    public StaffEntity getStaffByEmail(@RequestParam(defaultValue = "") String email) {
+        return staffService.findByMail(email);
+    }
+
     @GetMapping("{id}")
     @PreAuthorize("hasAuthority('VIEW_ITEM:STAFF:' + #id) or hasAuthority('VIEW_ALL:STAFF') or hasAuthority('ADMIN')")
     public StaffEntity getStaffById(@PathVariable String id) {
         return staffService.getOne(id);
     }
 
-    @PostMapping("")
-    @PreAuthorize("hasAuthority('CREATE:STAFF') or hasAuthority('ADMIN')")
-    public ResponseEntity<?> createStaff(@RequestBody NewStaffRequest newStaff) {
-        return ResponseEntity.ok(staffRepository.save(new StaffEntity(newStaff.getName(), newStaff.getPhone(), newStaff.getPassword(), newStaff.getEmail(), newStaff.getCitizenId(), newStaff.getRole())));
+    @GetMapping("{id}/permission")
+    public StaffPermissionResponse getPermsision(@PathVariable String id) {
+        return permissionService.getStaffPermission(id);
     }
     @PutMapping(path = "{id}")
-    public ResponseEntity<?>  edit(@RequestBody StaffDTO staffDTO, @PathVariable String id) {
+    public ResponseEntity<?>  edit(@RequestBody StaffDTO staffDTO, @PathVariable String id) throws IOException {
         return ResponseEntity.ok(staffService.update(staffDTO, id));
     }
     @PutMapping(path = "changePassword")
     public void changePass(@RequestParam String oldPassword, @RequestParam String newPassword, Principal connectedUser) throws Exception {
         staffService.changePassword(oldPassword, newPassword, connectedUser);
     }
+
     @DeleteMapping(path = "{id}")
     public void delete(@PathVariable String id) {
         staffService.remove(id);
