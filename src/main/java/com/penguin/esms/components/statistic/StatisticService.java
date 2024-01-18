33d94eae3ac57d1331e3429d36260f5d1d6b9287
AnhViet <anhviet0307@gmail.com>
2024-01-19 02:05:37 +0700

    GSOP-372 write add method in statistic service

--- src/main/java/com/penguin/esms/components/statistic/StatisticService.java
+++ src/main/java/com/penguin/esms/components/statistic/StatisticService.java
@@ -1,10 +1,22 @@
 package com.penguin.esms.components.statistic;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.penguin.esms.components.statistic.dto.StatisticDTO;
+import com.penguin.esms.utils.TimeUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class StatisticService {
     private final StatisticRepository repo;
+    public StatisticEntity add(StatisticDTO dto, Object object) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(object);
+        StatisticEntity entity = new StatisticEntity(dto.getName(), jsonString, new Date(), TimeUtils.getDay(new Date()));
+        return repo.save(entity);
+    }
 }
