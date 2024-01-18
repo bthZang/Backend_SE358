e7ef4d85e0da690b7603f3a45d37583d3f9105a5
AnhViet <anhviet0307@gmail.com>
2024-01-19 03:05:29 +0700

    GSOP-373 update category

---
+++ src/main/java/com/penguin/esms/components/category/response/FoundCategoryItem.java
@@ -0,0 +1,12 @@
+package com.penguin.esms.components.category.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FoundCategoryItem {
+    private String id;
+    private String name;
+}
