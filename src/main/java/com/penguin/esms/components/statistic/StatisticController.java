96645ac9830e16061028af6d34d893a74c866085
AnhViet <anhviet0307@gmail.com>
2024-01-19 02:26:48 +0700

    GSOP-372 get revenue by period

--- src/main/java/com/penguin/esms/components/statistic/StatisticController.java
+++ src/main/java/com/penguin/esms/components/statistic/StatisticController.java
@@ -1,7 +1,11 @@
 package com.penguin.esms.components.statistic;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("statistic")
@@ -13,6 +17,11 @@ public class StatisticController {
     @GetMapping("name/{name}")
     public StatisticEntity getName(@PathVariable String name) {
         return service.getByName(name);
+    }
+
+    @GetMapping("revenue")
+    public ResponseEntity<?> getRevenueByPeriod(@RequestParam long start, @RequestParam long end) throws JsonProcessingException {
+        return ResponseEntity.ok(service.getRevenuePeriod(new Date(start), new Date(end)));
     }
 
 }
--- src/main/java/com/penguin/esms/components/statistic/StatisticService.java
+++ src/main/java/com/penguin/esms/components/statistic/StatisticService.java
@@ -13,6 +13,7 @@ import java.util.*;
 @RequiredArgsConstructor
 public class StatisticService {
     private final StatisticRepository repo;
+
     public StatisticEntity add(String name, Object object) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonString = objectMapper.writeValueAsString(object);
@@ -23,4 +24,22 @@ public class StatisticService {
     public StatisticEntity getByName(String name) {
         return repo.findByName(name).get();
     }
+
+    public StatisticDTO getRevenuePeriod(Date start, Date end) throws JsonProcessingException {
+        StatisticDTO dto = new StatisticDTO(null, 0l, 0l, 0);
+        Map<String, StatisticDTO> map = new HashMap<>();
+        for (Long i = TimeUtils.getDay(start); i < TimeUtils.getDay(end); i++) {
+            Optional<StatisticEntity> statisticEntityOptional = repo.findByName("revenueByPeriod" + (i - 1) + i);
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                StatisticDTO statistic = objectMapper.readValue(statisticEntityOptional.get().getData(), StatisticDTO.class);
+                dto.setQuantity(dto.getQuantity() + statistic.getQuantity());
+                dto.setRevenue(dto.getRevenue() + statistic.getRevenue());
+            }catch (NoSuchElementException s){}
+
+        }
+        return dto;
+    }
+
+
 }
