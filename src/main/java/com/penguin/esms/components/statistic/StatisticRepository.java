5f7c6753f21008090f9098324b654d293a438025
AnhViet <anhviet0307@gmail.com>
2024-01-19 02:11:10 +0700

    GSOP-372 get statistic

--- src/main/java/com/penguin/esms/components/statistic/StatisticRepository.java
+++ src/main/java/com/penguin/esms/components/statistic/StatisticRepository.java
@@ -3,6 +3,10 @@ package com.penguin.esms.components.statistic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StatisticRepository extends JpaRepository<StatisticEntity, String> {
+    Optional<StatisticEntity> findById(String id);
+    Optional<StatisticEntity> findByName(String name);
 }
