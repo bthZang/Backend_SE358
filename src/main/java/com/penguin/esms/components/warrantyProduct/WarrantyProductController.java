63c3436970df734c901d95a03dcd12a40ea2edd2
AnhViet <anhviet0307@gmail.com>
2024-01-19 08:20:16 +0700

    GSOP-376 create data for warranty product

--- src/main/java/com/penguin/esms/components/warrantyProduct/WarrantyProductController.java
+++ src/main/java/com/penguin/esms/components/warrantyProduct/WarrantyProductController.java
@@ -1,6 +1,8 @@
 package com.penguin.esms.components.warrantyProduct;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path ="warranty_product")
 public class WarrantyProductController {
     private final WarrantyProductService service;
+    @GetMapping("{id}")
+    public WarrantyProductEntity getWarrantyProduct(@PathVariable String id) {
+        return service.getWarrantyProduct(id);
+    }
 }
--- src/main/java/com/penguin/esms/components/warrantyProduct/WarrantyProductService.java
+++ src/main/java/com/penguin/esms/components/warrantyProduct/WarrantyProductService.java
@@ -1,15 +1,57 @@
 package com.penguin.esms.components.warrantyProduct;
 
+import com.penguin.esms.components.product.ProductEntity;
+import com.penguin.esms.components.product.ProductRepo;
+import com.penguin.esms.components.product.ProductService;
+import com.penguin.esms.components.product.dto.ProductDTO;
+import com.penguin.esms.components.warrantyBill.WarrantyBillRepo;
+import com.penguin.esms.components.warrantyProduct.dto.WarrantyProductDTO;
+import com.penguin.esms.mapper.DTOtoEntityMapper;
+import com.penguin.esms.utils.Random;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Optional;
 
 @Service
 @Getter
 @Setter
 @RequiredArgsConstructor
 public class WarrantyProductService {
+    private final ProductService productService;
     private final WarrantyProductRepo repo;
+    private final ProductRepo productRepo;
+    private final WarrantyBillRepo warrantyRepo;
+    private final DTOtoEntityMapper mapper;
+    public WarrantyProductEntity getWarrantyProduct(String warrantyProductId) {
+        Optional<WarrantyProductEntity> warrantyProduct = repo.findById(warrantyProductId);
+        if (warrantyProduct.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
+        }
+        return warrantyProduct.get();
+    }
+    public WarrantyProductEntity getWarrantyBill(String warrantyBillId) {
+        Optional<WarrantyProductEntity> warrantyProduct = repo.findById(warrantyBillId);
+        if (warrantyProduct.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
+        }
+        return warrantyProduct.get();
+    }
+
+    public WarrantyProductDTO random() {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        String numbers = "123456789";
+        ProductDTO productDTO = productService.random();
+        ProductEntity productEntity = productService.add(productDTO);
+        String productId = productEntity.getId();
+        String warrantyContent = Random.random(20, characters);
+        String status = Random.random(20, characters);
+        String note = Random.random(20, characters);
+        Integer quantity = Integer.valueOf(Random.random(Integer.valueOf(Random.random(1, "12")), numbers));
+        return new WarrantyProductDTO(productId, quantity, warrantyContent, status, note);
+    }
 }
